4f74a5e33b86c1c4d4767234b293c4e8c90675eb
24digital <24digital@gmail.com>
2015-02-25 12:56:22 -0500

    d

--- app/src/main/java/com/example/marion/stocks/MainActivity.java
+++ app/src/main/java/com/example/marion/stocks/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.marion.stocks;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +13,6 @@ import android.widget.TextView;
 
 
 import java.io.IOException;
-import java.util.Queue;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -27,13 +26,10 @@ public class MainActivity extends ActionBarActivity {
         input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-               if(actionId == EditorInfo.IME_ACTION_DONE){
-                   try {
-                       stock.load();
-                   } catch (IOException e) {
-                       e.printStackTrace();
-                   }
-               }
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                  MyThread thread = new MyThread();
+                    thread.doInBackground(stock);
+                }
 
                 return false;
             }
@@ -47,23 +43,6 @@ public class MainActivity extends ActionBarActivity {
 
         }
 
-       String priceTemp  = stock.getLastTradePrice();
-       CharSequence charSequence = new String(priceTemp);
-
-    TextView priceValue = (TextView)findViewById(R.id.priceValue);
-        priceValue.setText(charSequence);
-
-        TextView rangeTemp = (TextView)findViewById(R.id.rangeView);
-        CharSequence raTemp = stock.getRange();
-       rangeTemp.setText(raTemp);
-
-        String changeTemp = stock.getChange();
-        TextView changeText = (TextView)findViewById(R.id.changeView);
-        changeText.setText((CharSequence)changeTemp);
-
-        String nameTemp = stock.getName();
-        TextView nameText = (TextView)findViewById(R.id.nameView);
-        nameText.setText((CharSequence)nameTemp);
 
 
     }
@@ -90,5 +69,68 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+        class MyThread extends AsyncTask<Stock,Integer, String>{
 
+
+            @Override
+            protected String doInBackground(Stock... params) {
+                try {
+                    stock.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                String priceTemp  = stock.getLastTradePrice();
+                CharSequence charSequence = new String(priceTemp);
+
+                TextView priceValue = (TextView)findViewById(R.id.priceValue);
+                priceValue.setText(charSequence);
+
+                TextView rangeTemp = (TextView)findViewById(R.id.rangeLabe);
+                CharSequence raTemp = stock.getRange();
+                rangeTemp.setText(raTemp);
+
+                String changeTemp = stock.getChange();
+                TextView changeText = (TextView)findViewById(R.id.changeView);
+                changeText.setText((CharSequence)changeTemp);
+
+                String nameTemp = stock.getName();
+                TextView nameText = (TextView)findViewById(R.id.nameView);
+                nameText.setText((CharSequence)nameTemp);
+
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+            }
+
+            @Override
+            protected void onCancelled(String s) {
+                super.onCancelled(s);
+            }
+        }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Stock stock = (Stock)savedInstanceState.getSerializable("obj");
+        if(stock !=null)
+            this.stock = stock;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("obj", (java.io.Serializable) stock);
+    }
 }
